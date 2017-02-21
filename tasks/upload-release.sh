d3aded15effcf2d7a944d7b6fbecbc4fd4bbda6a
Diego Lapiduz <dlapiduz@pivotal.io>
2017-02-21 10:03:55 -0600

    Change upload release task

--- tasks/upload-release.sh
+++ tasks/upload-release.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Hi"
+bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_ip`
+
+BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
+BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
+
+bosh login <<EOF 1>/dev/null
+$BOSH_USERNAME
+$BOSH_PASSWORD
+EOF
+
+bosh -n upload release prometheus-release/prometheus-*.tgz
