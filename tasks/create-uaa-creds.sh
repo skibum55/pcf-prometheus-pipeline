8807f42c49de2e5bc81ab09c82490bbd4d3b6eb9
Diego Lapiduz <dlapiduz@pivotal.io>
2017-02-21 14:32:56 -0600

    Use dlapiduz c0-worker-gcp

--- tasks/create-uaa-creds.sh
+++ tasks/create-uaa-creds.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-chmod +x tool-om/om-linux
-
-CURL="./tool-om/om-linux --target https://opsman.${pcf_ert_domain} -k \
+CURL="om --target https://opsman.${pcf_ert_domain} -k \
   --username $pcf_opsman_admin_username \
   --password $pcf_opsman_admin_password \
   curl"
--- tasks/get-bosh-creds.sh
+++ tasks/get-bosh-creds.sh
@@ -3,9 +3,7 @@ set -e
 
 mkdir -p get-bosh-creds
 
-chmod +x tool-om/om-linux
-
-CURL="./tool-om/om-linux --target https://opsman.${pcf_ert_domain} -k \
+CURL="om --target https://opsman.${pcf_ert_domain} -k \
   --username $pcf_opsman_admin_username \
   --password $pcf_opsman_admin_password \
   curl"
