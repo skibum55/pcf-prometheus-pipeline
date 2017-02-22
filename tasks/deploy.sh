bca889a6007ac5c76468aef36ff77a6b07d53307
Diego Lapiduz <dlapiduz@pivotal.io>
2017-02-21 20:07:07 -0600

    Add deploy task

---
+++ tasks/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_ip`
+
+BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
+BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
+
+bosh login <<EOF 1>/dev/null
+$BOSH_USERNAME
+$BOSH_PASSWORD
+EOF
+
+eval "echo \"$(cat tasks/etc/local.yml)\""
