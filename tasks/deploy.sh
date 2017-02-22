979356f6a19cc8290381037770534a6fd3321003
Diego Lapiduz <dlapiduz@pivotal.io>
2017-02-21 21:41:17 -0600

    Actually deploy

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 set -e
+CURL="om --target https://opsman.${pcf_ert_domain} -k \
+  --username $pcf_opsman_admin_username \
+  --password $pcf_opsman_admin_password \
+  curl"
 
 bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_ip`
 
 BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
 BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
 
+echo "Logging in to BOSH..."
 bosh login <<EOF 1>/dev/null
 $BOSH_USERNAME
 $BOSH_PASSWORD
 EOF
 
-eval "echo \"$(cat pcf-prometheus-git/tasks/etc/local.yml)\""
+echo "Getting NATS creds..."
+
+cf_id=$($CURL --path=/api/v0/deployed/products | jq -r ".[].guid" | grep cf-)
+
+nats_machines=$($CURL --path=/api/v0/deployed/products/$cf_id/status | \
+  jq -r -c  '.status[] | select(."job-name" | contains("nats")) | .ips' | \
+  perl -pe 's/\n//g' | perl -pe 's/\]\[/,/g')
+
+
+nats_creds=$($CURL --path=/api/v0/deployed/products/$cf_id/credentials/.nats.credentials)
+
+nats_username=$(echo $nats_creds | jq -r .credential.value.identity)
+nats_password=$(echo $nats_creds | jq -r .credential.value.password)
+
+echo "Interpolating..."
+
+eval "echo \"$(cat pcf-prometheus-git/tasks/etc/local.yml)\"" > local.yml
+
+cat local.yml
+
+bosh-cli interpolate pcf-prometheus-manifest/prometheus.yml -l local.yml > manifest.yml
+
+echo "Deploying..."
+
+bosh -n deployment manifest.yml
+
+bosh -n deploy --no-redact
--- tasks/upload-release.sh
+++ tasks/upload-release.sh
@@ -6,9 +6,14 @@ bosh -n --ca-cert om-bosh-creds/bosh-ca.pem target `cat om-bosh-creds/director_i
 BOSH_USERNAME=$(cat om-bosh-creds/bosh-username)
 BOSH_PASSWORD=$(cat om-bosh-creds/bosh-pass)
 
+echo "Logging in to BOSH..."
 bosh login <<EOF 1>/dev/null
 $BOSH_USERNAME
 $BOSH_PASSWORD
 EOF
 
+echo "Uploading Prometheus Release..."
 bosh -n upload release prometheus-release/prometheus-*.tgz
+
+echo "Uploading Routing Release..."
+bosh -n upload release https://bosh.io/d/github.com/cloudfoundry-incubator/cf-routing-release
