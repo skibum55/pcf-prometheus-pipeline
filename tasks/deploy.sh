f1be78e3ede086b5ab373bbb7ebea9a517846b30
Diego Lapiduz <dlapiduz@pivotal.io>
2017-04-12 11:47:44 -0500

    no routing

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -16,30 +16,9 @@ $BOSH_USERNAME
 $BOSH_PASSWORD
 EOF
 
-if [[ ! -z $opsman_url ]]; then
-  echo "Getting NATS creds..."
-
-  cf_id=$($CURL --path=/api/v0/deployed/products | jq -r ".[].guid" | grep cf-)
-
-  nats_machines=$($CURL --path=/api/v0/deployed/products/$cf_id/status | \
-    jq -r -c  '.status[] | select(."job-name" | contains("nats")) | .ips' | \
-    perl -pe 's/\n//g' | perl -pe 's/\]\[/,/g')
-
-
-  nats_creds=$($CURL --path=/api/v0/deployed/products/$cf_id/credentials/.nats.credentials)
-
-  nats_username=$(echo $nats_creds | jq -r .credential.value.identity)
-  nats_password=$(echo $nats_creds | jq -r .credential.value.password)
-
-  echo "Interpolating..."
-  eval "echo \"$(cat pcf-prometheus-git/tasks/etc/local.yml)\"" > local.yml
-  bosh-cli interpolate pcf-prometheus-manifest/prometheus.yml -l local.yml > manifest.yml
-else
-  echo "Interpolating..."
-  eval "echo \"$(cat pcf-prometheus-git/tasks/etc/local.yml)\"" > local.yml
-  bosh-cli interpolate pcf-prometheus-manifest/prometheus-no-route.yml -l local.yml > manifest.yml
-
-fi
+echo "Interpolating..."
+eval "echo \"$(cat pcf-prometheus-git/tasks/etc/local.yml)\"" > local.yml
+bosh-cli interpolate pcf-prometheus-manifest/prometheus-no-route.yml -l local.yml > manifest.yml
 
 
 echo "Deploying..."
